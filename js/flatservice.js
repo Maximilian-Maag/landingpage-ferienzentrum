2b9eb54b8dba09bb75a1775c0c658d3282f46ebf
Maximilian Jakob Maag <maximilian.maag@outlook.de>
2024-05-30 17:45:43 +0200

    flatservice and overview implementation

--- js/flatservice.js
+++ js/flatservice.js
@@ -16,7 +16,7 @@ function createFlatElement(Flat){
     flat.classList.add('col-12');
     flat.classList.add('mb-3');
     var thumbail = document.createElement("img");
-    //thumbail.attributes("src", Flat.thumbail);
+    thumbail.src = Flat.thumbail;
     thumbail.classList.add('img-fluid');
     thumbail.classList.add('product-image');
     flat.appendChild(thumbail);
@@ -24,16 +24,20 @@ function createFlatElement(Flat){
     productInfo.classList.add('product-info');
     productInfo.classList.add('d-flex')
     var productTitle = document.createElement("h5");
-    productTitle.innerHTML = document.createTextNode('Wohnung' + Flat.id);
+    let title = document.createTextNode('Nr. ' + Flat.id);
+    productTitle.appendChild(title);
     productTitle.classList.add('product-title');
     productTitle.classList.add('mb-0');
     var productDescription = document.createElement("p");
     productDescription.classList.add('product-p');
-    productDescription.innerHTML = document.createTextNode(Flat.description);
+    productDescription.classList.add('ms-auto');
+    let desc = document.createTextNode(Flat.description);
+    productDescription.appendChild(desc);
     var productPrice = document.createElement("small");
     productPrice.classList.add('text-muted');
     productPrice.classList.add('ms-auto');
-    productPrice.innerHTML = document.createTextNode(Flat.price+'€');
+    let price = document.createTextNode(Flat.price+' €/Nacht');
+    productPrice.appendChild(price);
     productInfo.appendChild(productTitle);
     productInfo.appendChild(productDescription);
     productInfo.appendChild(productPrice);
@@ -41,13 +45,38 @@ function createFlatElement(Flat){
     return flat;
 }
 
-var test = new Flat(56, 199, '/images/product/test.jpeg', 'www.google.de', '/images/product/', '2 Zimmer Küche/Bad');
-var flatElement = createFlatElement(test);
-console.log(flatElement);
 
-//var parent = document.getElementById('flatlist');
-//var test = document.createElement("p");
-//const testing = document.createTextNode("Just testing");
-//test.appendChild(testing);
-//parent.appendChild(test);
- 
+// Testing template only temporarly, will be replaced with json-parser
+var test = new Flat('57', '10', 'images/product/test.jpeg', 'www.google.de', '/images/product/', '2 Personen');
+var test2 = new Flat('2', '19', 'images/product/test2.jpeg', 'www.google.de', '/images/product/', '4 Personen');
+var test3 = new Flat('1111', '20000', 'images/product/test3.jpeg', 'www.google.de', '/images/product/', '20 Personen');
+var test4 = new Flat('33', '1', 'images/product/test4.jpeg', 'www.google.de', '/images/product/', '1 Person');
+var test5 = new Flat('12', '199', 'images/product/test5.jpeg', 'www.google.de', '/images/product/', '2 Personen');
+var test6 = new Flat('19', '199', 'images/product/test7.jpeg', 'www.google.de', '/images/product/', '2 Personen');
+var test7 = new Flat('22222', '199', 'images/product/test4.jpeg', 'www.google.de', '/images/product/', '2 Personen');
+var test8 = new Flat('1', '199', 'images/product/test4.jpeg', 'www.google.de', '/images/product/', '2 Personen');
+var test9 = new Flat('52', '199', 'images/product/test3.jpeg', 'www.google.de', '/images/product/', '2 Personen');
+var test10 = new Flat('76', '333333', 'images/product/test7.jpeg', 'www.google.de', '/images/product/', '2 Personen');
+
+var flatElement = createFlatElement(test);
+var flatElement2 = createFlatElement(test2);
+var flatElement3 = createFlatElement(test3);
+var flatElement4 = createFlatElement(test4);
+var flatElement5= createFlatElement(test5);
+var flatElement6 = createFlatElement(test6);
+var flatElement7 = createFlatElement(test6);
+var flatElement8 = createFlatElement(test8);
+var flatElement9 = createFlatElement(test9);
+var flatElement10 = createFlatElement(test10);
+
+var parent = document.getElementById('flatlist');
+parent.appendChild(flatElement);
+parent.appendChild(flatElement2);
+parent.appendChild(flatElement3);
+parent.appendChild(flatElement4);
+parent.appendChild(flatElement5);
+parent.appendChild(flatElement6);
+parent.appendChild(flatElement7);
+parent.appendChild(flatElement8);
+parent.appendChild(flatElement9);
+parent.appendChild(flatElement10);
