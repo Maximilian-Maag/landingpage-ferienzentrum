97f4bbaaa3168c46a58cc2111949dfdb44a212db
Maximilian Jakob Maag <maximilian.maag@outlook.de>
2024-12-27 15:07:57 +0100

    save

--- js/flatservice.js
+++ js/flatservice.js
@@ -50,7 +50,7 @@ function createFlatElement(Flat){
 var f1 = new Flat('1', '69', 'images/flats/f1/thumbmail.jpg', 'https://api.belegungskalender-kostenlos.de/kalender.php?kid=35565', 7, '45 qm 1 - 4 Personen');
 var f2 = new Flat('2', '39', 'images/flats/f2/thumbmail.jpg', 'https://api.belegungskalender-kostenlos.de/kalender.php?kid=35566', 6, '26 qm 1 - 2 Personen');
 var f3 = new Flat('3', '79', 'images/flats/f3/thumbmail.jpg', 'https://api.belegungskalender-kostenlos.de/kalender.php?kid=29061', 8, '2 Zimmer 57 qm 1 - 4 Personen');
-var f4 = new Flat('4', '69', 'images/flats/f4/thumbmail.jpg', 'https://api.belegungskalender-kostenlos.de/kalender.php?kid=34755', 6, '45 qm 1 - 4 Personen');
+//var f4 = new Flat('4', '69', 'images/flats/f4/thumbmail.jpg', 'https://api.belegungskalender-kostenlos.de/kalender.php?kid=34755', 6, '45 qm 1 - 4 Personen');
 var f5 = new Flat('5', '99', 'images/flats/f5/thumbmail.jpg', 'https://api.belegungskalender-kostenlos.de/kalender.php?kid=53749', 6, '2 Zimmer 70 qm 1 - 4 Personen');
 
 // generate html node from flat object
@@ -58,7 +58,7 @@ var f5 = new Flat('5', '99', 'images/flats/f5/thumbmail.jpg', 'https://api.beleg
 var f1_element = createFlatElement(f1);
 var f2_element = createFlatElement(f2);
 var f3_element = createFlatElement(f3);
-var f4_element = createFlatElement(f4);
+//var f4_element = createFlatElement(f4);
 var f5_element = createFlatElement(f5);
 
 // append html-nodes as childnodes in DOM-tree
@@ -66,7 +66,7 @@ var parent = document.getElementById('flatlist');
 parent.appendChild(f1_element);
 parent.appendChild(f2_element);
 parent.appendChild(f3_element);
-parent.appendChild(f4_element);
+//parent.appendChild(f4_element);
 parent.appendChild(f5_element);
 
 // gets called by modal service and returns flat data
@@ -75,7 +75,7 @@ function get_flat(id){
         case 1 : return f1;
         case 2 : return f2;
         case 3 : return f3;
-        case 4 : return f4;
+        //case 4 : return f4;
         case 5 : return f5;
     }
 }
